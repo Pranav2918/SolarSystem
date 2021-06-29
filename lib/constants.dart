41f0ffa46d93a21fdaae9de3178856d62b01dfaa
Pranav Dave <pranavdave18@gmail.com>
2021-06-29 18:21:14 +0530

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = Color(0xff868686);
+Color navigationColor = Color.fromRGBO(255, 0, 21, 0.9);
+Color gradientStartColor = Color.fromRGBO(255, 0, 21, 0.9);
+Color gradientEndColor = Colors.red[200];
